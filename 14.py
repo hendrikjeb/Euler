2cfe3db6a65352c33edf98c38ed94b1095065f06
Hendrikje Balkenende <hendrikjeb@example.org>
2015-12-19 15:14:30 +0100

    14.2: Performance verbeterd (ruim 3 sec)
    Vanaf nu wordt voor ieder getal dat in een Collatz-reeks wordt gevonden, in een lijst gezet hoelang de reeks van dat getal is. Daardoor kan sneller gevonden worden hoelang de reeks is. Het duurt nu ruim 3 seconden.

--- 14.py
+++ 14.py
@@ -17,34 +17,53 @@ Which starting number, under one million, produces the longest chain?
 
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
-# Tijd:  44.3609998226
+# Tijd:  3.09
 
 from time import time
 start = time()
 
-lijst = range(0, 1000000)
-maxm = 0
+lijst = [0, 1] + 10000000 * [0]
+maxm = [1, 1]
 
-for x in lijst:
-	teller = 1
-	if x == 0:
-		continue
-	else:
-		y = x
-		while x > 1:
-			teller += 1
-			if x % 2 == 0:
-				x /= 2
-			else:
-				x = 3 * x + 1
+bugfix = time() # Voorals bij start alleen 999999 wordt opgeschreven...
+
+for start in range(2, 1000000):
+	# controleer of de lengte van de Collatz-reeks voor een getal al is gevonden
+	if lijst[start] == 0:
+		x = start
+		tijdelijk = []
+		while lijst[start] == 0:
+
+		# Maak een tijdelijke lijst met alle getallen die je vindt. 
+		# Vervang daarbij de getallen die buiten de index van de grote lijst 
+		# vallen door 0.
+			while True:
+				try:
+					if lijst[x] != 0:
+						break
+					else:
+						tijdelijk.append(x)
+				except IndexError:
+					tijdelijk.append(0)
+				
+				if x % 2 == 0:
+					x /= 2
+				else:
+					x = 3 * x + 1	
 			
-			try:
-				lijst[x] = 0
-			except IndexError:
-				pass
+			collatz = lijst[x]
 
-		if maxm < teller:
-			maxm = teller
-			print y, ":", maxm
+			# Onderstaande loop zorgt dat voor alle gevonden getallen, in de lijst
+			# komt te staan hoelang hun Collatzreeks is.
+			while len(tijdelijk) != 0:
+				collatz += 1
+				z = tijdelijk.pop()
+				# print "lijst[%d] = %d" % (z, collatz)
+				lijst[z] = collatz
+
+		if maxm[1] < lijst[start]:
+			maxm = [start, lijst[start]]
+			print "%d => %d" % (maxm[0], maxm[1])
 
 print 'Tijd: ', time() - start
+print 'Tijd: ', time() - bugfix
