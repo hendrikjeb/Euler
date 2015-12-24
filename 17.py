852cf3c34d008c56939ea27ab2c83862df0ff851
Hendrikje Balkenende <hendrikjeb@example.org>
2015-12-24 19:36:06 +0100

    17.1: Printen
    Eerst alle getallen printen.

--- 17.py
+++ 17.py
@@ -17,6 +17,55 @@ The use of "and" when writing out numbers is in compliance with British usage.
 from time import time
 start = time()
 
+getallen = {
+1: "one",
+2: "two",
+3: "three",
+4: "four",
+5: "five",
+6: "six",
+7: "seven",
+8: "eight",
+9: "nine",
+10: "ten",
+11: "eleven",
+12: "twelve",
+13: "thirteen",
+14: "fourteen",
+15: "fifteen",
+16: "sixteen",
+17: "seventeen",
+18: "eighteen",
+19: "nineteen",
+20: "twenty",
+30: "thirty",
+40: "forty",
+50: "fifty",
+60: "sixty",
+70: "seventy",
+80: "eighty",
+90: "ninety"
+}
 
+def maakgetal(g):
+	if g < 20:
+		print getallen[g]
+	elif g > 19 and g < 100:
+		if g % 10 != 0:
+			tiental = g - (g % 10)
+			print getallen[tiental] + "-" + getallen[g % 10]
+		else:
+			print getallen[g]
+
+for x in range(1, 100):
+	maakgetal(x)
+
+for y in range(1, 10):
+	print getallen[y], "hundred"
+	for z in range(1, 100):
+		print getallen[y], "hundred", "and",
+		maakgetal(z)
+
+print "one", "thousand"
 
 print 'Tijd: ', time() - start
