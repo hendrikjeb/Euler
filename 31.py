cf5a78a246beb4eea9b34df12b4fa2bc165fb8fa
Hendrikje Balkenende <hendrikjeb@example.org>
2016-05-11 07:05:27 +0200

    31.2: Opgelost met heel veel loopjes

--- 31.py
+++ 31.py
@@ -17,25 +17,43 @@ How many different ways can £2 be made using any number of coins?
 from time import time
 start = time()
 
-munten = [200, 100, 50, 10, 5, 2, 1]
-lijst = [[200]]
+t2 = 1
+max100, t100 = 200, 0
 
-x = 0
-while True:
-	try:
-		x += 1
-		lijst.append([])
-		_200 = 200
-		y = 0
-		while _200 > 0:
-			if _200 < munten[y] or munten[y] == lijst[x - 1][-1]:
-				y += 1
-			else:
-				lijst[x].append(munten[y])
-				_200 -= munten[y]
-				print munten[y]
-	except IndexError:
-		print "!"
-		break
+while max100 >= 0:
+	max50, t50 = max100, 0
+
+	while max50 >= 0:
+		max20, t20 = max50, 0
+
+		while max20 >= 0:
+			max10, t10 = max20, 0
+
+			while max10 >= 0:
+				max5, t5 = max10, 0
+
+				while max5 >= 0:
+					max2 = max5
+					
+					while max2 >= 0:
+						max2 -= 2
+						t2 += 1
+					
+					max5 -= 5
+					t5 += 1
+				
+				max10 -= 10
+				t10 += 1		
+
+			max20 -= 20
+			t20 += 1
+		
+		max50 -= 50
+		t50 += 1
+
+	max100 -= 100
+	t100 += 1
+
+print t2
 
 print 'Tijd: ', time() - start
